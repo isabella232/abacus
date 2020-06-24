dbf54086e74ed8851b2b6b5a25f52606eaa37b53
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-23 23:32:21 -0600

    Exclude Any Function for Resulting Type (#157)

--- types/ExcludeMethods.ts
+++ types/ExcludeMethods.ts
@@ -1,4 +1,4 @@
 /**
  * Allows to create a type with all the same properties as `T`, excluding methods.
  */
-export type ExcludeMethods<T> = Pick<T, { [K in keyof T]: T[K] extends (_: unknown) => unknown ? never : K }[keyof T]>
+export type ExcludeMethods<T> = Pick<T, { [K in keyof T]: T[K] extends Function ? never : K }[keyof T]>
