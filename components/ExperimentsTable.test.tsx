6e929c2a9cba2e5eed188a04aa307013e3c7b1e9
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-07-03 23:36:09 -0600

    Remove unnecessary pagination testing (#191)

--- components/ExperimentsTable.test.tsx
+++ components/ExperimentsTable.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, getAllByText, getByText, getByTitle } from '@testing-library/react'
+import { getByText } from '@testing-library/react'
 import addToDate from 'date-fns/add'
 import React from 'react'
 
@@ -42,58 +42,4 @@ test('with one page of experiments, renders a table', () => {
   expect(getByText(tBodyElmt, 'First', { selector: 'tr > td' })).toBeInTheDocument()
   expect(getByText(tBodyElmt, 'wpcom', { selector: 'tr > td' })).toBeInTheDocument()
   expect(getByText(tBodyElmt, 'Owner', { selector: 'tr > td' })).toBeInTheDocument()
-})
-
-test('with more than one page of experiments, renders a table with a pagination control', () => {
-  const PER_PAGE = 25
-  const COUNT = 31 // Some value greater than "per page".
-  const startDatetime = new Date()
-  const endDatetime = addToDate(new Date(), { days: 14 })
-  const experiments: ExperimentBare[] = Array.from(Array(COUNT).keys()).map((num) => ({
-    experimentId: num + 1,
-    name: `Name${num + 1}`,
-    endDatetime,
-    ownerLogin: 'Owner',
-    platform: Platform.Wpcom,
-    startDatetime,
-    status: Status.Staging,
-  }))
-
-  const { container } = render(<ExperimentsTable experiments={experiments} />)
-
-  let tBodyElmt = container.querySelector('tbody') as HTMLTableSectionElement
-  expect(tBodyElmt).not.toBeNull()
-  expect(getByText(tBodyElmt, 'Name1', { selector: 'tr > td' })).toBeInTheDocument()
-  expect(getAllByText(tBodyElmt, 'wpcom', { selector: 'tr > td' })).toHaveLength(PER_PAGE)
-  expect(getAllByText(tBodyElmt, 'Owner', { selector: 'tr > td' })).toHaveLength(PER_PAGE)
-
-  const tFootElmt = container.querySelector('tfoot') as HTMLTableSectionElement
-  expect(tFootElmt).not.toBeNull()
-  const nextBtnContainer = getByTitle(tFootElmt, 'Next Page')
-  expect(nextBtnContainer).not.toBeNull()
-  const nextBtn = nextBtnContainer.querySelector('button') as HTMLButtonElement
-
-  // Click "next" button
-  fireEvent.click(nextBtn)
-
-  // Should be on the second page now.
-  tBodyElmt = container.querySelector('tbody') as HTMLTableSectionElement
-  expect(tBodyElmt).not.toBeNull()
-  expect(getByText(tBodyElmt, 'Name26', { selector: 'tr > td' })).toBeInTheDocument()
-  expect(getAllByText(tBodyElmt, 'wpcom', { selector: 'tr > td' })).toHaveLength(COUNT - PER_PAGE)
-  expect(getAllByText(tBodyElmt, 'Owner', { selector: 'tr > td' })).toHaveLength(COUNT - PER_PAGE)
-
-  const prevBtnContainer = getByTitle(tFootElmt, 'Previous Page')
-  expect(prevBtnContainer).not.toBeNull()
-  const prevBtn = prevBtnContainer.querySelector('button') as HTMLButtonElement
-
-  // Click "page 1" button
-  fireEvent.click(prevBtn)
-
-  // Should be back on the first page again.
-  tBodyElmt = container.querySelector('tbody') as HTMLTableSectionElement
-  expect(tBodyElmt).not.toBeNull()
-  expect(getByText(tBodyElmt, 'Name1', { selector: 'tr > td' })).toBeInTheDocument()
-  expect(getAllByText(tBodyElmt, 'wpcom', { selector: 'tr > td' })).toHaveLength(PER_PAGE)
-  expect(getAllByText(tBodyElmt, 'Owner', { selector: 'tr > td' })).toHaveLength(PER_PAGE)
 })
