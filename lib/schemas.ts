c0a3a1dbe52d931ef3e17c76c7c5a6246edac7c3
Jessie Ross <jessie.ross@automattic.com>
2020-07-27 13:50:25 +0800

    Add experiment normalizr schema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -121,6 +121,11 @@ export const metricAssignmentSchema = metricAssignmentNewSchema
   .defined()
   .camelCase()
 export type MetricAssignment = yup.InferType<typeof metricAssignmentSchema>
+export const metricAssignmentNormalizrSchema = new normalizr.schema.Entity<MetricAssignment>(
+  'metricAssignments',
+  {},
+  { idAttribute: 'metricAssignmentId' },
+)
 
 export enum SegmentType {
   Country = 'country',
@@ -154,6 +159,11 @@ export const segmentAssignmentSchema = segmentAssignmentNewSchema
   .defined()
   .camelCase()
 export type SegmentAssignment = yup.InferType<typeof segmentAssignmentSchema>
+export const segmentAssignmentNormalizrSchema = new normalizr.schema.Entity<SegmentAssignment>(
+  'segmentAssignments',
+  {},
+  { idAttribute: 'segmentAssignmentId' },
+)
 
 export const variationNewSchema = yup
   .object({
@@ -172,6 +182,11 @@ export const variationSchema = variationNewSchema
   .defined()
   .camelCase()
 export type Variation = yup.InferType<typeof variationSchema>
+export const variationNormalizrSchema = new normalizr.schema.Entity<Variation>(
+  'variations',
+  {},
+  { idAttribute: 'variationId' },
+)
 
 export enum Platform {
   Calypso = 'calypso',
@@ -217,6 +232,15 @@ export const experimentFullSchema = experimentBareSchema
   .defined()
   .camelCase()
 export type ExperimentFull = yup.InferType<typeof experimentFullSchema>
+export const experimentFullNormalizrSchema = new normalizr.schema.Entity<ExperimentFull>(
+  'experiments',
+  {
+    metricsAssignments: [metricAssignmentNormalizrSchema],
+    segmentAssignments: [segmentAssignmentNormalizrSchema],
+    variations: [variationNormalizrSchema],
+  },
+  { idAttribute: 'experimentId' },
+)
 
 const now = new Date()
 export const experimentFullNewSchema = experimentFullSchema.shape({
