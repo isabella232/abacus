1ad8a184e63f3afdb8e6e5b4fad5499b50abe903
Jessie Ross <jessie.ross@automattic.com>
2020-07-31 12:53:17 +0800

    Extract auth callback code to auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,7 @@
 import debugFactory from 'debug'
-import { toInt } from 'qc-to_int'
-import qs from 'querystring'
 import React, { useEffect, useState } from 'react'
 
-import { saveExperimentsAuthInfo } from '@/utils/auth'
+import { AuthError, onExperimentAuthCallbackUrl } from '@/utils/auth'
 
 const debug = debugFactory('abacus:pages/auth.tsx')
 
@@ -17,33 +15,9 @@ const AuthPage = function AuthPage() {
   debug('AuthPage#render')
   const [error, setError] = useState<null | string>(null)
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      // If we have the hash with the authorization info, then extract the info, save
-      // it for later, and go to the "home" page.
-      //
-      // The hash should look something like the following upon success:
-      // #access_token=...&expires_in=#######&scope=global&site_id=0&token_type=bearer
-      // The hash should look something like the following upon failure:
-      // #error=access_denied&error_description=You+need+to+log+in+to+WordPress.com&state=
-      if (window.location.hash && window.location.hash.length > 1) {
-        const authInfo = qs.parse(window.location.hash.substring(1))
-        if (authInfo.access_token && authInfo.scope === 'global' && authInfo.token_type === 'bearer') {
-          const expiresInSeconds = toInt(authInfo.expires_in)
-          const experimentsAuthInfo = {
-            accessToken: authInfo.access_token as string,
-            expiresAt: typeof expiresInSeconds === 'number' ? Date.now() + expiresInSeconds * 1000 : null,
-            scope: 'global',
-            type: 'bearer',
-          }
-          saveExperimentsAuthInfo(experimentsAuthInfo)
-
-          window.location.replace(window.location.origin)
-        } else if (authInfo.error === 'access_denied') {
-          setError('Please log into WordPress.com and authorize Abacus - Testing to have access.')
-
-          saveExperimentsAuthInfo(null)
-        }
-      }
+    const error = onExperimentAuthCallbackUrl()
+    if (error === AuthError.AccessDenied) {
+      setError('Please log into WordPress.com and authorize Abacus - Testing to have access.')
     }
   }, [])
 
--- utils/auth.ts
+++ utils/auth.ts
@@ -1,3 +1,4 @@
+import { toInt } from 'qc-to_int'
 import qs from 'querystring'
 
 import { config } from '../config'
@@ -72,4 +73,46 @@ export function initializeExperimentsAuth() {
   console.info('InitializeExperimentAuth: Could not find existing auth info, re-authing.')
   const authUrl = `${config.experimentApi.authPath}?${qs.stringify(authQuery)}`
   window.location.replace(authUrl)
+}
+
+export enum AuthError {
+  AccessDenied,
+}
+
+/* istanbul ignore next; TODO: e2e test authorization */
+export function onExperimentAuthCallbackUrl(): void | AuthError {
+  if (typeof window === 'undefined') {
+    console.warn('onExperimentAuthCallbackUrl: Could not find `window`')
+    return
+  }
+
+  if (!window.location.hash || window.location.hash.length === 0) {
+    throw new Error('Missing hash in auth callback url.')
+  }
+
+  // If we have the hash with the authorization info, then extract the info, save
+  // it for later, and go to the "home" page.
+  //
+  // The hash should look something like the following upon success:
+  // #access_token=...&expires_in=#######&scope=global&site_id=0&token_type=bearer
+  // The hash should look something like the following upon failure:
+  // #error=access_denied&error_description=You+need+to+log+in+to+WordPress.com&state=
+  const authInfo = qs.parse(window.location.hash.substring(1))
+  if (authInfo.error === 'access_denied') {
+    saveExperimentsAuthInfo(null)
+    return AuthError.AccessDenied
+  }
+
+  if (authInfo.access_token && authInfo.scope === 'global' && authInfo.token_type === 'bearer') {
+    const expiresInSeconds = toInt(authInfo.expires_in)
+    const experimentsAuthInfo = {
+      accessToken: authInfo.access_token as string,
+      expiresAt: typeof expiresInSeconds === 'number' ? Date.now() + expiresInSeconds * 1000 : null,
+      scope: 'global',
+      type: 'bearer',
+    }
+    saveExperimentsAuthInfo(experimentsAuthInfo)
+
+    window.location.replace(window.location.origin)
+  }
 }
