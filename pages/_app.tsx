e2efcb92c54737f27c5722a42fa090c9f7be98ba
Jessie Ross <jessie.ross@automattic.com>
2020-07-31 11:41:48 +0800

    Move auth initialization to auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,13 @@ import { makeStyles } from '@material-ui/core/styles'
 import debugFactory from 'debug'
 import { AppProps } from 'next/app'
 import { SnackbarProvider } from 'notistack'
-import qs from 'querystring'
 import React from 'react'
 import * as yup from 'yup'
 
 import RenderErrorBoundary from '@/components/RenderErrorBoundary'
 import RenderErrorView from '@/components/RenderErrorView'
 import ThemeProvider from '@/styles/ThemeProvider'
-import { getAuthClientId, getExperimentsAuthInfo, saveExperimentsAuthInfo } from '@/utils/auth'
+import { initializeExperimentsAuthentication } from '@/utils/auth'
 
 const debug = debugFactory('abacus:pages/_app.tsx')
 
@@ -92,33 +91,7 @@ const App = React.memo(function App(props: AppProps) {
     }
   }, [])
 
-  if (typeof window !== 'undefined') {
-    // Inject a fake auth token to skip authentication in non-production contexts.
-    // This is a temporary solution. Ideally, we should test the full authentication flow in every environment.
-    if (window.location.host !== 'experiments.a8c.com') {
-      saveExperimentsAuthInfo({
-        accessToken: 'fake_token',
-        expiresAt: Date.parse('2100-01-01'),
-        scope: 'global',
-        type: 'bearer',
-      })
-    }
-
-    // Prompt user for authorization if we don't have auth info.
-    const experimentsAuthInfo = getExperimentsAuthInfo()
-    if (!experimentsAuthInfo) {
-      const authPath = 'https://public-api.wordpress.com/oauth2/authorize'
-      const authQuery = {
-        client_id: getAuthClientId(window.location.host),
-        redirect_uri: `${window.location.origin}/auth`,
-        response_type: 'token',
-        scope: 'global',
-      }
-
-      const authUrl = `${authPath}?${qs.stringify(authQuery)}`
-      window.location.replace(authUrl)
-    }
-  }
+  initializeExperimentsAuthentication()
 
   return (
     <RenderErrorBoundary>
--- utils/auth.ts
+++ utils/auth.ts
@@ -1,3 +1,5 @@
+import qs from 'querystring'
+
 /**
  * Resolves the OAuth client ID based on the host.
  *
@@ -44,5 +46,35 @@ export const saveExperimentsAuthInfo = (experimentsAuthInfo: ExperimentsAuthInfo
     localStorage.removeItem('experiments_auth_info')
   } else {
     localStorage.setItem('experiments_auth_info', JSON.stringify(experimentsAuthInfo))
+  }
+}
+
+export function initializeExperimentsAuthentication() {
+  if (typeof window !== 'undefined') {
+    // Inject a fake auth token to skip authentication in non-production contexts.
+    // This is a temporary solution. Ideally, we should test the full authentication flow in every environment.
+    if (window.location.host !== 'experiments.a8c.com') {
+      saveExperimentsAuthInfo({
+        accessToken: 'fake_token',
+        expiresAt: Date.parse('2100-01-01'),
+        scope: 'global',
+        type: 'bearer',
+      })
+    }
+
+    // Prompt user for authorization if we don't have auth info.
+    const experimentsAuthInfo = getExperimentsAuthInfo()
+    if (!experimentsAuthInfo) {
+      const authPath = 'https://public-api.wordpress.com/oauth2/authorize'
+      const authQuery = {
+        client_id: getAuthClientId(window.location.host),
+        redirect_uri: `${window.location.origin}/auth`,
+        response_type: 'token',
+        scope: 'global',
+      }
+
+      const authUrl = `${authPath}?${qs.stringify(authQuery)}`
+      window.location.replace(authUrl)
+    }
   }
 }
