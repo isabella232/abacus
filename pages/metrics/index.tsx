5d5b15bf8b415a464003ba8913101b29d574ee1e
Jessie Ross <j@scjr.me>
2020-08-21 15:37:01 +0800

    Add metric CRUD modal state (#267)
    * Add edit metric button
    
    * Add "Add Metric" button
    
    * Fix tests
    
    * Fix formatting
    
    * Add Edit metric state
    
    * Add "Add Metric" state
    
    * Fix formatting
    
    * Change ternary positions
    
    * Remove file-level eslint supression
    
    * Fix dataLoadingError for editMetric
    
    * Fix formatting

--- pages/metrics/index.tsx
+++ pages/metrics/index.tsx
@@ -1,11 +1,12 @@
 import { Button, createStyles, LinearProgress, makeStyles, Theme } from '@material-ui/core'
 import debugFactory from 'debug'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 
 import MetricsApi from '@/api/MetricsApi'
 import Layout from '@/components/Layout'
 import MetricsTable from '@/components/MetricsTable'
+import { MetricParameterType } from '@/lib/schemas'
 import { useDataLoadingError, useDataSource } from '@/utils/data-loading'
 
 const debug = debugFactory('abacus:pages/metrics/index.tsx')
@@ -30,8 +31,37 @@ const MetricsIndexPage = () => {
   const router = useRouter()
   const debugMode = router.query.debug === 'true'
 
-  const onEditMetric = (metricId: number) => alert(metricId)
-  const onAddMetric = () => alert('add metric')
+  // Edit Metric Modal
+  const [editMetricMetricId, setEditMetricMetricId] = useState<number | null>(null)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const isEditingMetric = editMetricMetricId !== null
+  const {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    isLoading: editMetricIsLoading,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    data: editMetricInitialMetric,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    error: editMetricError,
+  } = useDataSource(async () => {
+    return editMetricMetricId === null ? null : await MetricsApi.findById(editMetricMetricId)
+  }, [editMetricMetricId])
+  useDataLoadingError(editMetricError, 'Metric to edit')
+  const onEditMetric = (metricId: number) => {
+    setEditMetricMetricId(metricId)
+  }
+
+  // Add Metric Modal
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isAddingMetric, setIsAddingMetric] = useState<boolean>(false)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const addMetricInitialMetric = {
+    name: '',
+    description: '',
+    parameterType: MetricParameterType.Conversion,
+    higherIsBetter: 'true',
+    params: '',
+  }
+  const onAddMetric = () => setIsAddingMetric(true)
 
   return (
     <Layout title='Metrics'>
