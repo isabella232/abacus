02a6dcf3ffc41a096dd1bf1a0fa4c0635dbeb9a1
Jessie Ross <jessie.ross@automattic.com>
2020-09-09 00:26:40 +0800

    Refactor ExperimentPageView tests

--- components/ExperimentPageView.test.tsx
+++ components/ExperimentPageView.test.tsx
@@ -36,8 +36,8 @@ mockedNotistack.useSnackbar.mockImplementation(() => ({
   closeSnackbar: jest.fn(),
 }))
 
-test('staging experiment shows correct features enabled in overview', async () => {
-  const experiment = Fixtures.createExperimentFull({ status: Status.Staging })
+const renderExperimentPageView = (status: Status) => {
+  const experiment = Fixtures.createExperimentFull({ status })
   mockedExperimentsApi.findById.mockImplementationOnce(async () => experiment)
 
   const metrics = Fixtures.createMetricBares(10)
@@ -49,13 +49,18 @@ test('staging experiment shows correct features enabled in overview', async () =
   const analyses = Fixtures.createAnalyses()
   mockedAnalysesApi.findByExperimentId.mockImplementationOnce(async () => analyses)
 
-  const { container } = render(
+  return render(
     <ExperimentPageView experimentId={experiment.experimentId} view={ExperimentView.Overview} debugMode={false} />,
   )
+}
 
-  await waitFor(() => screen.getByRole('button', { name: /Assign Metric/ }))
-  expect(container).toMatchSnapshot()
-
+/**
+ * Gets an map of button states
+ * - true: visible and enabled
+ * - false: visible and disabled
+ * - null: not on screen
+ */
+const getButtonStates = () => {
   const editInWizard = screen.getByRole('link', { name: /Edit In Wizard/ })
   const run = screen.getByRole('button', { name: /Run/ })
   const disable = screen.getByRole('button', { name: /Disable/ })
@@ -63,115 +68,72 @@ test('staging experiment shows correct features enabled in overview', async () =
   const assignMetric = screen.getByRole('button', { name: /Assign Metric/ })
   const conclusionEdit = screen.queryByRole('button', { name: /Edit Conclusion/ })
 
-  expect(editInWizard.classList.contains('Mui-disabled')).toBe(false)
-  expect(run.hasAttribute('disabled')).toBe(false)
-  expect(disable.hasAttribute('disabled')).toBe(false)
-  expect(generalPanelEdit.hasAttribute('disabled')).toBe(true)
-  expect(assignMetric.hasAttribute('disabled')).toBe(true)
-  expect(conclusionEdit).toBeNull()
+  return {
+    editInWizard: !editInWizard.classList.contains('Mui-disabled'),
+    run: !run.hasAttribute('disabled'),
+    disable: !disable.hasAttribute('disabled'),
+    generalPanelEdit: !generalPanelEdit.hasAttribute('disabled'),
+    assignMetric: !assignMetric.hasAttribute('disabled'),
+    conclusionEdit: conclusionEdit && !conclusionEdit.hasAttribute('disabled'),
+  }
+}
+
+test('staging experiment shows correct features enabled in overview', async () => {
+  const { container: _container } = renderExperimentPageView(Status.Staging)
+
+  await waitFor(() => screen.getByRole('button', { name: /Assign Metric/ }))
+
+  expect(getButtonStates()).toEqual({
+    editInWizard: true,
+    run: true,
+    disable: true,
+    generalPanelEdit: false,
+    assignMetric: false,
+    conclusionEdit: null,
+  })
 })
 
 test('running experiment shows correct features enabled in overview', async () => {
-  const experiment = Fixtures.createExperimentFull({ status: Status.Running })
-  mockedExperimentsApi.findById.mockImplementationOnce(async () => experiment)
-
-  const metrics = Fixtures.createMetricBares(10)
-  mockedMetricsApi.findAll.mockImplementationOnce(async () => metrics)
-
-  const segments = Fixtures.createSegments(10)
-  mockedSegmentsApi.findAll.mockImplementationOnce(async () => segments)
-
-  const analyses = Fixtures.createAnalyses()
-  mockedAnalysesApi.findByExperimentId.mockImplementationOnce(async () => analyses)
-
-  const { container } = render(
-    <ExperimentPageView experimentId={experiment.experimentId} view={ExperimentView.Overview} debugMode={false} />,
-  )
+  const { container: _container } = renderExperimentPageView(Status.Running)
 
   await waitFor(() => screen.getByRole('button', { name: /Assign Metric/ }))
-  expect(container).toMatchSnapshot()
 
-  const editInWizard = screen.getByRole('link', { name: /Edit In Wizard/ })
-  const run = screen.getByRole('button', { name: /Run/ })
-  const disable = screen.getByRole('button', { name: /Disable/ })
-  const generalPanelEdit = screen.getByRole('button', { name: /Edit Experiment General Data/ })
-  const assignMetric = screen.getByRole('button', { name: /Assign Metric/ })
-  const conclusionEdit = screen.queryByRole('button', { name: /Edit Conclusion/ })
-
-  expect(editInWizard.classList.contains('Mui-disabled')).toBe(true)
-  expect(run.hasAttribute('disabled')).toBe(true)
-  expect(disable.hasAttribute('disabled')).toBe(false)
-  expect(generalPanelEdit.hasAttribute('disabled')).toBe(false)
-  expect(assignMetric.hasAttribute('disabled')).toBe(false)
-  expect(conclusionEdit).toBeNull()
+  expect(getButtonStates()).toEqual({
+    editInWizard: false,
+    run: false,
+    disable: true,
+    generalPanelEdit: true,
+    assignMetric: true,
+    conclusionEdit: null,
+  })
 })
 
 test('completed experiment shows correct features enabled in overview', async () => {
-  const experiment = Fixtures.createExperimentFull({ status: Status.Completed })
-  mockedExperimentsApi.findById.mockImplementationOnce(async () => experiment)
-
-  const metrics = Fixtures.createMetricBares(10)
-  mockedMetricsApi.findAll.mockImplementationOnce(async () => metrics)
-
-  const segments = Fixtures.createSegments(10)
-  mockedSegmentsApi.findAll.mockImplementationOnce(async () => segments)
-
-  const analyses = Fixtures.createAnalyses()
-  mockedAnalysesApi.findByExperimentId.mockImplementationOnce(async () => analyses)
-
-  const { container } = render(
-    <ExperimentPageView experimentId={experiment.experimentId} view={ExperimentView.Overview} debugMode={false} />,
-  )
+  const { container: _container } = renderExperimentPageView(Status.Completed)
 
   await waitFor(() => screen.getByRole('button', { name: /Assign Metric/ }))
-  expect(container).toMatchSnapshot()
 
-  const editInWizard = screen.getByRole('link', { name: /Edit In Wizard/ })
-  const run = screen.getByRole('button', { name: /Run/ })
-  const disable = screen.getByRole('button', { name: /Disable/ })
-  const generalPanelEdit = screen.getByRole('button', { name: /Edit Experiment General Data/ })
-  const assignMetric = screen.getByRole('button', { name: /Assign Metric/ })
-  const conclusionEdit = screen.getByRole('button', { name: /Edit Conclusion/ })
-
-  expect(editInWizard.classList.contains('Mui-disabled')).toBe(true)
-  expect(run.hasAttribute('disabled')).toBe(true)
-  expect(disable.hasAttribute('disabled')).toBe(false)
-  expect(generalPanelEdit.hasAttribute('disabled')).toBe(false)
-  expect(assignMetric.hasAttribute('disabled')).toBe(false)
-  expect(conclusionEdit.hasAttribute('disabled')).toBe(false)
+  expect(getButtonStates()).toEqual({
+    editInWizard: false,
+    run: false,
+    disable: true,
+    generalPanelEdit: true,
+    assignMetric: true,
+    conclusionEdit: true,
+  })
 })
 
 test('disabled experiment shows correct features enabled in overview', async () => {
-  const experiment = Fixtures.createExperimentFull({ status: Status.Disabled })
-  mockedExperimentsApi.findById.mockImplementationOnce(async () => experiment)
-
-  const metrics = Fixtures.createMetricBares(10)
-  mockedMetricsApi.findAll.mockImplementationOnce(async () => metrics)
-
-  const segments = Fixtures.createSegments(10)
-  mockedSegmentsApi.findAll.mockImplementationOnce(async () => segments)
-
-  const analyses = Fixtures.createAnalyses()
-  mockedAnalysesApi.findByExperimentId.mockImplementationOnce(async () => analyses)
-
-  const { container } = render(
-    <ExperimentPageView experimentId={experiment.experimentId} view={ExperimentView.Overview} debugMode={false} />,
-  )
+  const { container: _container } = renderExperimentPageView(Status.Disabled)
 
   await waitFor(() => screen.getByRole('button', { name: /Assign Metric/ }))
-  expect(container).toMatchSnapshot()
 
-  const editInWizard = screen.getByRole('link', { name: /Edit In Wizard/ })
-  const run = screen.getByRole('button', { name: /Run/ })
-  const disable = screen.getByRole('button', { name: /Disable/ })
-  const generalPanelEdit = screen.getByRole('button', { name: /Edit Experiment General Data/ })
-  const assignMetric = screen.getByRole('button', { name: /Assign Metric/ })
-  const conclusionEdit = screen.getByRole('button', { name: /Edit Conclusion/ })
-
-  expect(editInWizard.classList.contains('Mui-disabled')).toBe(true)
-  expect(run.hasAttribute('disabled')).toBe(true)
-  expect(disable.hasAttribute('disabled')).toBe(true)
-  expect(generalPanelEdit.hasAttribute('disabled')).toBe(false)
-  expect(assignMetric.hasAttribute('disabled')).toBe(false)
-  expect(conclusionEdit.hasAttribute('disabled')).toBe(false)
+  expect(getButtonStates()).toEqual({
+    editInWizard: false,
+    run: false,
+    disable: false,
+    generalPanelEdit: true,
+    assignMetric: true,
+    conclusionEdit: true,
+  })
 })
