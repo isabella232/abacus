0b8859d5ab37edd68e343818d4bfd67f686b534e
Jessie Ross <jessie.ross@automattic.com>
2020-09-22 15:49:29 +0800

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -38,7 +38,7 @@ module.exports = {
   },
   preset: 'ts-jest',
   // Adds special extended assertions to Jest, thus simplifying the tests.
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', '<rootDir>/test-helpers/unit-test-setup.ts'],
   testPathIgnorePatterns: ['/__tests__/', '/e2e/', '/node_modules/'],
   testTimeout: 60000,
   transform: {
