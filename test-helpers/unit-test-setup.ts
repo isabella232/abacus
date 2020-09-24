a381cf0654cf7d43a55bebe1465bb89f2e86ee78
Jessie Ross <j@scjr.me>
2020-09-25 04:53:27 +0800

    Revert "Revert "Add basic result graphs (#334)" (#340)" (#342)

---
+++ test-helpers/unit-test-setup.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
+// @ts-ignore
+window.URL.createObjectURL = jest.fn()
+
+// @ts-ignore
+window.HTMLCanvasElement.prototype.getContext = jest.fn()
