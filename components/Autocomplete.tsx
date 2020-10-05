fae66989c5ca4103577c6a8d26d2f4df79780368
Rob Landers <landers.robert@gmail.com>
2020-10-05 13:19:14 +0200

    Add an autocomplete component (#346)
    * Create autocomplete component
    
    * Remove interface
    
    * Add comment explaining why the functions are broken out
    
    * Update the comment about these here functions

---
+++ components/Autocomplete.tsx
@@ -0,0 +1,50 @@
+import { CircularProgress } from '@material-ui/core'
+import { Autocomplete } from '@material-ui/lab'
+import { AutocompleteProps, AutocompleteRenderInputParams, fieldToAutocomplete } from 'formik-material-ui-lab'
+import React, { useCallback } from 'react'
+
+import { AutocompleteItem } from '@/lib/schemas'
+
+// We need to handle the cases where the form is initialized with a string, or when we have a specific value selected.
+// Also, it's possible that the option is null, though typescript asserts that it isn't possible.
+export const getOptionValue = (option: AutocompleteItem | string | null) =>
+  (typeof option === 'string' ? option : option?.value) ?? ''
+
+export const getOptionLabel = (option: AutocompleteItem | string | null) =>
+  (typeof option === 'string' ? option : option?.name) ?? ''
+
+export const getOptionSelected = (option: AutocompleteItem, value: string | AutocompleteItem) =>
+  typeof value === 'string' ? value === option.value : value.value === option.value
+
+export const autocompleteAttributes = {
+  getOptionLabel: getOptionLabel,
+  getOptionSelected: getOptionSelected,
+}
+
+export const autocompleteInputProps = (params: AutocompleteRenderInputParams, loading: boolean) => {
+  return {
+    ...params.InputProps,
+    endAdornment: (
+      <>
+        {loading ? <CircularProgress color='inherit' size={20} /> : null}
+        {params.InputProps.endAdornment}
+      </>
+    ),
+  }
+}
+
+export default function AbacusAutocomplete(props: AutocompleteProps<AutocompleteItem, false, false, false>) {
+  const {
+    form: { setFieldValue },
+    field: { name },
+  } = props
+
+  const onChange = useCallback(
+    (ev, option: AutocompleteItem | string | null) => {
+      setFieldValue(name, getOptionValue(option))
+    },
+    [setFieldValue, name],
+  )
+
+  return <Autocomplete {...fieldToAutocomplete(props)} {...autocompleteAttributes} onChange={onChange} />
+}
