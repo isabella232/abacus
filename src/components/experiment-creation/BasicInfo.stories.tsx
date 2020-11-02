ce859b38e4936f3921718c2eada625d3895ed701
Jessie Ross <jessie.ross@automattic.com>
2020-11-02 23:50:03 +0800

    Fix stories

--- src/components/experiment-creation/BasicInfo.stories.tsx
+++ src/components/experiment-creation/BasicInfo.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { getEventNameCompletions, getUserCompletions } from 'src/api/AutocompleteApi'
+import { AutocompleteItem } from 'src/lib/schemas'
 import { MockFormik } from 'src/test-helpers/test-utils'
 import { useDataSource } from 'src/utils/data-loading'
 
@@ -12,6 +13,7 @@ export const FormPart = (): JSX.Element => {
   const completionBag = {
     userCompletionDataSource: useDataSource(getUserCompletions, []),
     eventCompletionDataSource: useDataSource(getEventNameCompletions, []),
+    exclusionGroupCompletionDataSource: useDataSource<AutocompleteItem[], [], Error>(() => new Promise((resolve) => resolve([])), [])
   }
   return (
     <MockFormik>
