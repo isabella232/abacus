a0ff874c0650b4d9ac98c948d8857faf1a6d4c0f
Jessie Ross <jessie.ross@automattic.com>
2020-11-10 16:52:48 +0800

    Add new routes

---
+++ src/Routes.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+
+import Experiment from 'src/pages-real/experiments/Experiment'
+import ExperimentNew from 'src/pages-real/experiments/ExperimentNew'
+import Experiments from 'src/pages-real/experiments/Experiments'
+import ExperimentWizardEdit from 'src/pages-real/experiments/ExperimentWizardEdit'
+import Metrics from 'src/pages-real/Metrics'
+import Tags from 'src/pages-real/Tags'
+
+/**
+ * Let's keep routing simple and minimal.
+ * - Do not use dynamic or nested routing!
+ * - Get all your route information at the top level.
+ * - Try not to delete or change an existing route: comment a route as deprecated and redirect.
+ */
+function Routes(): JSX.Element {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/experiments' />
+        </Route>
+
+        <Route path='/experiments' exact>
+          <Experiments />
+        </Route>
+
+        <Route path='/experiments/new' exact>
+          <ExperimentNew />
+        </Route>
+        <Route path='/experiments/:experimentId' exact>
+          {/* Relative redirecting is buggy because of NextJS: */}
+          {/* <Redirect to="./overview" /> */}
+          <Experiment />
+        </Route>
+        <Route path='/experiments/:experimentId/wizard-edit' exact>
+          <ExperimentWizardEdit />
+        </Route>
+        <Route path='/experiments/:experimentId/:view' exact>
+          <Experiment />
+        </Route>
+
+        <Route path='/metrics' exact>
+          <Metrics />
+        </Route>
+
+        <Route path='/tags' exact>
+          <Tags />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+// NextJS: Allows this to not unmount between routes
+export default React.memo(Routes)
