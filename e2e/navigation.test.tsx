56b92eb0df1754da80e2add2649d6c766c498a58
Jessie Ross <jessie.ross@automattic.com>
2020-11-11 18:15:44 +0800

    Revert "Fix tests"
    This reverts commit 317c3616a773859400226bb6f39106a73923bf5d.

--- e2e/navigation.test.tsx
+++ e2e/navigation.test.tsx
@@ -33,10 +33,10 @@ describe('Experiments', () => {
       // Click an experiment row.
       const $tableRows = await page.$$('.MuiTableBody-root .MuiTableRow-root')
       expect($tableRows.length).toBeGreaterThan(0)
-      await $tableRows[0].click()
+      await Promise.all([page.waitForNavigation(), $tableRows[0].click()])
 
       // Assert clicking a row navigated to the details page of that experiment.
-      expect(await page.evaluate(() => location.href)).toMatch(/^http:\/\/a8c-abacus-local:3001\/experiments\/\d+/)
+      expect(page.url()).toMatch(/^http:\/\/a8c-abacus-local:3001\/experiments\/\d+/)
     })
   })
 })
