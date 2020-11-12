215d224249b6e12264548aafb304c211e1f270b8
Jessie Ross <jessie.ross@automattic.com>
2020-11-12 19:24:32 +0800

    Increase timeout

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -8,7 +8,7 @@ module.exports = {
     command:
       'echo "Building app..." && REACT_APP_E2E_TEST_BUILD=true npm run build && echo "Starting app..." && npm run start -- -l 3001',
     debug: true, // Allows us to see the output of the above commands.
-    launchTimeout: 180 * 1000,
+    launchTimeout: 300 * 1000,
     port: 3001,
   },
 }
